32a7c8944e43c5408e0fd1227bedf9e5887a6947
Massimo Centemeri <massimo.centemeri@evolutionpeople.it>
2022-12-17 01:49:32 +0100

    comment

--- SpinWords/SpinWords.go
+++ SpinWords/SpinWords.go
@@ -5,7 +5,11 @@ import (
 )
 
 /*
-Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+Write a function that takes in a string of one or more words,
+and returns the same string, but with all five or more letter words reversed
+(Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
 
 Examples:
 
